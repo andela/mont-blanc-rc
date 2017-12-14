48b2543093951fee6ff3a13fb6efeed922f521f8
Paradise Kelechi <paradisekelechi@gmail.com>
2017-12-14 21:10:27 +0100

    feature(order): implement cancel order
    [delivers #152798768]

--- imports/plugins/core/checkout/client/components/cancelOrderButton.jsx
+++ imports/plugins/core/checkout/client/components/cancelOrderButton.jsx
@@ -1,20 +1,25 @@
 import React, { PropTypes } from 'react';
 
 const CancelOrderButton = (props) => {
+
   const { order } = props;
-  if (order.workflow.status !== 'cancelled') {
-    return (
-      <div className="row cancel-button-row">
-        <div className="col-md-12">
-          <button className="btn btn-block btn-danger cancel-button">Cancel Order</button>
-        </div>
-      </div>
-    );
-  }
+  return (
+    <div>
+      {order.workflow.status !== 'cancelled' ?
+        (<div className="row cancel-button-row">
+          <div className="col-md-12">
+            <button onClick={props.onCancelOrderClick} className="btn btn-block btn-danger cancel-button">Cancel Order</button>
+          </div>
+        </div>) :
+        ' '
+      }
+    </div>
+  );
 };
 
 CancelOrderButton.propTypes = {
-  order: PropTypes.object.isRequired
+  order: PropTypes.object.isRequired,
+  onCancelOrderClick: PropTypes.func.isRequired
 };
 
 export default CancelOrderButton;
