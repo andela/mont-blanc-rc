8d32b9fca7e204e51af611d0d074598e7d570c3c
Balogun Abiodun Delight <delighteddell@gmail.com>
2017-12-14 18:23:58 +0100

    feature(onbaord-users): add a brief tour to onboard users

---
+++ imports/plugins/included/tour/customerTour.js
@@ -0,0 +1,52 @@
+
+
+const tour = [
+  {
+    intro: `<h3>Welcome to Reaction Commerce</h3>
+        <hr>
+        <div>I would like to take you on a quick tour of how to use this application</div>`
+  },
+  {
+    element: '.product-grid',
+    intro: `<h3>Products Gallery</h3>
+        <hr>
+        <div> Here you can browse through our gallery of available products and add the products you'll like to purchase to your cart</div>`
+  },
+  {
+    element: '.cart-container',
+    intro: `<h3> Shopping Cart </h3>
+        <hr>
+        <div> Your shopping cart contains all the products you selected for purchase. Click on checkout button to proceed to checkout</div>`
+  },
+  {
+    element: '.accounts',
+    intro: `<h3> User Account Options</h3>
+        <hr>
+        <div>You can access user account options. You can also register, signin or signout here</div>`
+  },
+  {
+    element: '.currencies',
+    intro: `<h3> Currency Options</h3>
+        <hr>
+        <div> Select your prefered currency for making payment</div>`
+  },
+  {
+    element: '.languages',
+    intro: `<h3> Language Options</h3>
+        <hr>
+        <div> You can select your prefered language by clicking on this button </div>`
+  },
+  {
+    element: '.search',
+    intro: `<h3>Search Products</h3>
+        <hr>
+        <div> Search for your desired products by clicking on this icon</div>`
+  },
+  {
+    intro: `<h3>End of Tour</h3>
+        <hr>
+        <div>Hope the tour was helpful? Why not get started with shopping.</div>`
+  }
+];
+
+export default tour;
