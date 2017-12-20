8d9caa37c0d8debc6263292fb747dc12185eac0a
Balogun Abiodun Delight <delighteddell@gmail.com>
2017-12-20 10:44:06 +0100

    bug(onboard-users): modify and fix little bugs tour

--- imports/plugins/included/tour/shopOwnerTour.js
+++ imports/plugins/included/tour/shopOwnerTour.js
@@ -7,25 +7,15 @@ const tour = [
           <div>I would like to take you on a quick tour of how to use this application</div>`
   },
   {
-    element: '.product-grid',
-    intro: `<h3>Products Gallery</h3>
-          <hr>
-          <div> Here you can browse through your product gallery</div>`
-  },
-  {
-    element: '.toolbar-vertical',
-    intro: `<h3>Vendor Dashboard</h3>
-            <hr>
-            <div>Here you can manage your store, attend to orders, choose your preferred payment methods etc.</div>`
-  },
-  {
     element: '.cart-container',
     intro: `<h3> Shopping Cart </h3>
           <hr>
-          <div> Your shopping cart contains all the products you selected for purchase. Click on checkout button to proceed to checkout</div>`
+          <div> Your shopping cart contains all the products you selected for purchase. Click on checkout button to proceed to checkout</div>`,
+    position: 'bottom'
+
   },
   {
-    element: 'dropdown-toggle',
+    element: '.accounts',
     intro: `<h3> User Account Options</h3>
           <hr>
           <div>You can access user account options. You can also register, signin or signout here</div>`
@@ -49,16 +39,29 @@ const tour = [
           <div> Search for your desired products by clicking on this icon</div>`
   },
   {
+    element: '.toolbar-vertical',
+    intro: `<h3>Vendor Dashboard</h3>
+            <hr>
+            <div>Here is your dashboard. You can manage your store, attend to orders, choose your preferred payment methods, etc.</div>`
+  },
+  {
     element: 'nav.rui',
     intro: `<h3> Toolbar</h3>
           <hr>
           <div> You can carry out admin operations like adding, editing and viewing products from this section</div>`
   },
   {
+    element: '.product-grid',
+    intro: `<h3>Products Gallery</h3>
+          <hr>
+          <div>This is the product gallery. Any product you upload to your store will be seen here.</div>`,
+    position: 'top'
+  },
+  {
     element: '.left',
     intro: `<h3> Toggle Modes</h3>
           <hr>
-          <div> Toggle between edit and view mode to make and view changes to your shop</div>`
+          <div> After you must have uploaded products, here you can make changes to your products, like changing the image, price, quantity, etc. Toggle between edit and view mode to make and view changes to your shop or products</div>`
   },
   {
     intro: `<h3>End of Tour</h3>
